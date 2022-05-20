99c6b984b83ceebc932050f35378e50a8497ab73
AlexYach <alexyach1973@gmail.com>
2022-05-20 12:09:05 +0300

    Lesson 6

--- app/src/main/java/com/alexyach/geekbrains/android/mynotes/NoteDetailsFragment.java
+++ app/src/main/java/com/alexyach/geekbrains/android/mynotes/NoteDetailsFragment.java
@@ -2,18 +2,16 @@ package com.alexyach.geekbrains.android.mynotes;
 
 import android.os.Build;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.annotation.RequiresApi;
-import androidx.fragment.app.Fragment;
-
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.DatePicker;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.fragment.app.Fragment;
 
 import java.util.List;
 
